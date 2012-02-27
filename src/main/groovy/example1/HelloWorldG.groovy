6b0791ef056c89e8d95e30d47bcee45dbe8986c7
Iyori Daisuke <iyoridaisuke@gmail.com>
2012-02-27 11:30:01 +0900

    もっとも簡単なGroovyサンプルを追加
    あまり参考にならない？GroovyとJavaの比較

---
+++ src/main/groovy/example1/HelloWorldG.groovy
@@ -0,0 +1,2 @@
+package example1
+println 'Hello,World'
---
+++ src/main/java/example1/HelloWorldJ.java
@@ -0,0 +1,8 @@
+package example1;
+
+public class HelloWorldJ {
+	public static final void main(String[] args) {
+		System.out.println("Hello,World");
+	}
+}
+
